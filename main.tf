f0ade17b6094ef07bb831deb1143b7e67336f13c
Mathis DI MASCIO <mathis.dimascio@gmail.com>
2022-10-12 13:22:50 +0200

    rename ec2 and bucket

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 module "ec2_instance" {
   source            = "./ec2_instance"
   my_ip             = chomp(data.http.myip.body)
-  instance_name     = "peter-ec2"
+  instance_name     = "ezeqielle-ec2"
   subnet_id         = module.network.new_subnet_id
   security_group_id = module.network.security_group_id
 }
 
 module "s3_bucket" {
   source                        = "./s3_bucket"
-  bucket_name                   = "peter-bucket"
+  bucket_name                   = "ezeqielle-bucket"
   very_secret_access_key_id     = module.iam.access_key_id
   very_secret_access_key_secret = module.iam.access_key_secret
   very_secret_username          = module.iam.username
